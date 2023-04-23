95c5f74f7db8269276e3b04ce250cfd09589792b
Reanne Bernardo <reannemaebernardo@gmail.com>
2023-04-23 17:47:04 +0800

    update forms
    added "add another project" button
    added hr

--- js/demo/datatables-demo.js
+++ js/demo/datatables-demo.js
@@ -2,3 +2,11 @@
 $(document).ready(function() {
   $('#dataTable').DataTable();
 });
+
+// $('#addItem').click(function() {
+//   var contents = $('.itemGroup').html();
+//   $('.itemGroup').append(contents);
+//   $('.itemGroup').append(
+//     '<a href="#" class="btn btn-danger mb-3" id="removeItem">Remove Item</a>'
+//   );
+// });
